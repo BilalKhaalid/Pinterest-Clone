30a6cbca50aa6679cbec0c415d9832d821c40077
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-18 16:30:03 +0500

    posts model created

---
+++ server/routes/posts.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+// Define Post Schema
+const postSchema = new mongoose.Schema({
+  postText: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  likes: {
+    type: Array,
+    default: [],
+  },
+  // Assuming you have a User model, linking the post to an author
+  //   author: {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: "User",
+  //     required: true,
+  //   },
+});
+
+// Create Post model
+const Post = mongoose.model("Post", postSchema);
+
+// Export Post model
+module.exports = Post;
