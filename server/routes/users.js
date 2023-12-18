e10cd05b31c576689a51a1dedb8c8bf24b58d6f5
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-18 16:39:11 +0500

    intended the code

--- server/routes/users.js
+++ server/routes/users.js
@@ -8,11 +8,25 @@ mongoose.connect("mongodb://localhost/pinterest", {
 
 // Define User Schema
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
   posts: [], // Assuming you have a Post model
-  fullName: { type: String, required: true },
+  fullName: {
+    type: String,
+    required: true,
+  },
 });
 
 // Create User model
