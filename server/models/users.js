35c5282d5c1cfb0e6d1cd49713ec7adac053cab8
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-19 12:50:42 +0500

    connecting to mongo setup

--- server/models/users.js
+++ server/models/users.js
@@ -1,6 +1,16 @@
-var express = require("express");
 const mongoose = require("mongoose");
 const plm = require("passport-local-mongoose");
+
+const mongoURI = "mongodb://localhost:27017";
+
+mongoose.connect(mongoURI);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
 
 const UserSchema = new mongoose.Schema({
   email: {
