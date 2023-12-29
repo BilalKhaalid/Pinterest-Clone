d10689eb64a4a0a267868b2c59644f92a8b925a7
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-29 17:08:39 +0500

    added a user field for storing author's ids

--- data-association/routes/posts.js
+++ data-association/routes/posts.js
@@ -13,6 +13,10 @@ const postSchema = new mongoose.Schema({
     type: Array,
     default: [],
   },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 module.exports = mongoose.model("Post", postSchema);
