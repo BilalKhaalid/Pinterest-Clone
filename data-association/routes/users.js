ae5191a3c5f3bc2103aa228e4fcbd68b76f70389
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-29 16:23:04 +0500

    Created User model

--- data-association/routes/users.js
+++ data-association/routes/users.js
@@ -4,3 +4,33 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/data-association");
 
 // Define User Schema
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+  dp: {
+    type: String, // Assuming that the DP (display picture) is a URL or a file path
+  },
+  fullname: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
