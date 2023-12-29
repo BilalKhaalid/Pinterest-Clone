77b381d8ad318506cd32a3fc2ad7f29e9697c845
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-29 16:20:12 +0500

    cleaned for practice

--- data-association/routes/users.js
+++ data-association/routes/users.js
@@ -4,35 +4,3 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/data-association");
 
 // Define User Schema
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  posts: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Post",
-    },
-  ], // Assuming you have a Post model
-  fullName: {
-    type: String,
-    required: true,
-  },
-});
-
-// Create User model
-const User = mongoose.model("User", userSchema);
-
-// Export User model
-module.exports = User;
