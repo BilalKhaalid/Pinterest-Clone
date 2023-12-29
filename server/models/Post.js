1da78234b6b98dbf5053a524987124f8deac1ce7
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-29 20:51:05 +0500

    Created Model for Posts

---
+++ server/models/Post.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  postText: {
+    type: String,
+    required: true,
+  },
+  currentDateAndTime: {
+    type: Date,
+    default: Date.now,
+  },
+  likes: {
+    type: Array,
+    default: [],
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+module.exports = mongoose.model("Posts", postSchema);
