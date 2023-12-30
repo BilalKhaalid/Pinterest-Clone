a219a82772cacf4020c6316af769d54a1ff9b389
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-30 17:15:42 +0500

    made changes to posts model name to post

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-  postText: {
+  imageText: {
     type: String,
     required: true,
   },
@@ -17,6 +17,9 @@ const postSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
   },
+  image: {
+    type: String,
+  },
 });
 
-module.exports = mongoose.model("Posts", postSchema);
+module.exports = mongoose.model("Post", postSchema);
